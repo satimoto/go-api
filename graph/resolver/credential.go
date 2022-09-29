8768d4635d0b25fd43c96691caa6a2513392eeee
Ross Savage <ross.savage@agrista.com>
2022-09-29 09:43:35 +0200

    Harden admin API requests. Closes #15

--- graph/resolver/credential.go
+++ graph/resolver/credential.go
@@ -16,7 +16,7 @@ import (
 )
 
 func (r *mutationResolver) CreateCredential(ctx context.Context, input graph.CreateCredentialInput) (*db.Credential, error) {
-	if userId := middleware.GetUserId(ctx); userId != nil {
+	if user := middleware.GetUser(ctx, r.UserRepository); user != nil && user.IsAdmin {
 		credentialRequest := credential.NewCreateCredentialRequest(input)
 		credentialResponse, err := r.OcpiService.CreateCredential(ctx, credentialRequest)
 
@@ -33,7 +33,7 @@ func (r *mutationResolver) CreateCredential(ctx context.Context, input graph.Cre
 }
 
 func (r *mutationResolver) RegisterCredential(ctx context.Context, input graph.RegisterCredentialInput) (*graph.ResultID, error) {
-	if userId := middleware.GetUserId(ctx); userId != nil {
+	if user := middleware.GetUser(ctx, r.UserRepository); user != nil && user.IsAdmin {
 		credentialRequest := credential.NewRegisterCredentialRequest(input)
 		credentialResponse, err := r.OcpiService.RegisterCredential(ctx, credentialRequest)
 
@@ -50,7 +50,7 @@ func (r *mutationResolver) RegisterCredential(ctx context.Context, input graph.R
 }
 
 func (r *mutationResolver) UnregisterCredential(ctx context.Context, input graph.UnregisterCredentialInput) (*graph.ResultID, error) {
-	if userId := middleware.GetUserId(ctx); userId != nil {
+	if user := middleware.GetUser(ctx, r.UserRepository); user != nil && user.IsAdmin {
 		credentialRequest := credential.NewUnregisterCredentialRequest(input)
 		credentialResponse, err := r.OcpiService.UnregisterCredential(ctx, credentialRequest)
 
